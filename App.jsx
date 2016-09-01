88d1d8176bb0c1fd1f32a6bcb32936ffae25650f
Rose Wang <rose.wang@appian.com>
2016-09-01 19:21:19 -0400

    Making a Component
    Move render logic for Message from App.jsx to Message.jsx. Then import
    Message.jsx to render Messages within App.jsx.

--- App.jsx
+++ App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as firebase from 'firebase'; // Import Firebase library
+import Message from './components/Message.jsx';
 
 // Data to authenticate Firebase
 const firebaseConfig = {
@@ -51,7 +52,7 @@ const App = React.createClass({
   },
 
   renderMessageDiv(message) {
-    return <p key={message.key}>{message.name}: {message.message}</p>;
+    return <Message key={message.key} message={message}/>;
   },
 
   render() {
