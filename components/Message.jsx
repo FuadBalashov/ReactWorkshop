6d6b5de0674b45dcd0380649d7352569bf00072f
Michael Deng <michael.deng@appian.com>
2016-11-15 09:33:13 -0500

    Starter Project

---
+++ components/Message.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Message = React.createClass({
+  // Setting propTypes ensure that your component is used correctly
+  propTypes: {
+    message: React.PropTypes.object
+  },
+
+  render() {
+  }
+});
+
+export default Message;
